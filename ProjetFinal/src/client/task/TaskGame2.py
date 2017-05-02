55d6343baa261ef9570ab928efa8961cb582e5e4
Means1 <mathieu-pain@orange.fr>
2017-05-02 22:40:22 +0200

    d

--- ProjetFinal/src/client/task/TaskGame2.py
+++ ProjetFinal/src/client/task/TaskGame2.py
@@ -25,7 +25,7 @@ class TaskGame2(threading.Thread):
         self.text3 = None
         self.text4 = None
 
-    # ~ Fonction run de la thread
+        # ~ Fonction run de la thread
     def run(self):
         time.sleep(1)
         while (self.main.running and self.main.ingame):
@@ -46,21 +46,25 @@ class TaskGame2(threading.Thread):
 
             if (self.personne in self.main.fenetregame.findlist and self.count != -1):
                 self.count += 1
-               
-            
+
+
             elif (len(self.main.fenetregame.findlist) != 0):
                 if (self.main.fenetregame.other[self.main.fenetregame.findlist[0]][4] == 1):
                     self.personne = self.main.fenetregame.findlist[0]
                     self.count = 0
-                    self.text2 = self.main.writeText(650, 635, "[                                                                  ]", self.main.fenetregame.canvas, False, 15, '#D9D526')
-                    
+                    msg = "["
+                    i = 0
+                    while i < 20:
+                        msg+=" "
+                    self.text2 = self.main.writeText(650, 635, msg + "]", self.main.fenetregame.canvas, False, 15, '#D9D526')
+                    print(str((self.count/50)*20))
             else:
                 if (self.text2 != None):
                     self.main.fenetregame.canvas.delete(self.text2)
-                    
+
                 if (self.text3 != None):
                     self.main.fenetregame.canvas.delete(self.text3)
-                    
+
                 if (self.text4 != None):
                     self.main.fenetregame.canvas.delete(self.text4)
                 self.count = 0
